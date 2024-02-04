5e0b955261d8e4f4932381e52a5f4f8b8969bdb4
Ubuntu <ubuntu@ip-172-31-22-187.ap-northeast-2.compute.internal>
2024-02-04 19:46:50 +0000

    # ADD - docker start shell

---
+++ docker-start.sh
@@ -0,0 +1,29 @@
+#sudo docker build -t realworld-api .
+
+#sudo docker run -d -p 8080:8080 realworld-api
+
+#!/bin/bash
+
+# 실행 중인 컨테이너의 이름 또는 ID 설정
+CONTAINER_NAME="realworld-api"
+
+# 실행 중인 동일한 이름의 컨테이너가 있는지 확인
+if [ $(sudo docker ps -q -f ancestor=${CONTAINER_NAME}) ]; then
+	echo "qwejioqwjeoiqwejoiqwej"
+	# 컨테이너가 실행 중이면 중지하고 삭제
+	echo "Stopping and removing existing container..."
+	sudo docker stop ${CONTAINER_NAME}
+	sudo docker rm ${CONTAINER_NAME}
+fi
+
+echo ${CONTAINER_NAME}a
+
+# Docker 이미지 빌드
+echo "Building Docker image..."
+sudo docker build -t realworld-api .
+
+# 새로운 컨테이너 실행
+echo "Running new container..."
+sudo docker run -d -p 8080:8080 --name ${CONTAINER_NAME} realworld-api
+
+echo "Container ${CONTAINER_NAME} is running."
